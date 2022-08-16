671c6aafb478cd7a90c53df630e62c0864128913
Python The Hack <lrulvoni@gmail.com>
2022-08-16 12:36:39 +0200

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //template: 'Paccheri al sugo',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ViewProOne';
+  description = "una nuova app realizzata da LR";
 }
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+  titolo:string = 'Shining';
+  autore: string = 'SK';
+  src: string = '../../assets/imgs/book.jpg';
+
+  titolo2: string = 'Programming';
+  autore2: string = 'Coom';
+  src2: string = '../../assets/imgs/book.jpg';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
